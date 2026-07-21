16a2a4149af4defa0526316d7baee52d259ba66b
ImaMAG <125838015+Ima-MAG@users.noreply.github.com>
2026-07-21 03:05:21 +0100

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        l1 = []
+        l2 = []
+
+        for i in s:
+            l1.append(i)
+
+        for i in t:
+            l2.append(i)
+
+        l1.sort()
+        l2.sort()
+
+        if l1 == l2:
+            return True
+        return False
