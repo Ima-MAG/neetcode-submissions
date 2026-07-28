175585375f8b2ae007532e557fafd6b0f10f01b5
ImaMAG <125838015+Ima-MAG@users.noreply.github.com>
2026-07-28 20:59:21 +0100

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        data: dict[str, list] = {}
+
+        for i in strs:
+            sortedWord = ''.join(sorted(i))
+            if not sortedWord in data:
+                data[sortedWord] = [i]
+            else:
+                data[sortedWord].append(i)
+
+        return list(data.values())
